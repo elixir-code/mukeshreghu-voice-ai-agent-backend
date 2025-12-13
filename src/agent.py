75e68ee8bface05f4b305acdccb83e883351b878
R Mukesh <ced15i002@iiitdm.ac.in>
2025-12-13 09:17:11 +0000

    Rename model and remove lang arg for Assembly AI 1.2 plugin

--- src/agent.py
+++ src/agent.py
@@ -68,7 +68,7 @@ async def my_agent(ctx: JobContext):
     session = AgentSession(
         # Speech-to-text (STT) is your agent's ears, turning the user's speech into text that the LLM can understand
         # See all available models at https://docs.livekit.io/agents/models/stt/
-        stt=assemblyai.STT(model="universal-streaming", language="en"),
+        stt=assemblyai.STT(model="universal-streaming-english"),
         # A Large Language Model (LLM) is your agent's brain, processing user input and generating a response
         # See all available models at https://docs.livekit.io/agents/models/llm/
         llm=openai.LLM(model="gpt-5-nano"),
